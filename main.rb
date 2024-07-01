a2f959a3a62a159fc567e6f3b5c13b83d9c7b81f
Michał Bielawski <michal.bielawski02@gmail.com>
2024-07-01 23:33:01 +0200

    last few cases to handle in remove_at and insert_at methods

--- main.rb
+++ main.rb
@@ -53,6 +53,82 @@ class LinkedList
     node = Node.new(value, @head.next_node)
     @head.next_node = node
   end
+
+  # Trzeba dodać kilka przypadków.
+  def insert_at(value, index)
+    node = Node.new(value, at(index))
+    if index == 0
+      @head.next_node = node
+    else
+      at(index - 1).next_node = node
+    end
+  end
+
+  def remove_at(index)
+    return if empty?
+    return if at(index).nil?
+
+    node = at(index)
+    at(index - 1).next_node = at(index + 1) unless index == 0
+    @head.next_node = at(index + 1)
+    node.next_node = nil
+  end
+
+  def each(node = @head.next_node, &block)
+    return if node == @tail
+
+    raise 'No block given.' unless block_given?
+
+    yield node
+    each(node.next_node, &block)
+  end
+
+  def at(index, node = @head.next_node, i = 0)
+    return node if index == i
+    return nil if node.nil?
+
+    at(index, node.next_node, i + 1)
+  end
+
+  def pop(node = @head)
+    if size.zero?
+      nil
+    elsif node.next_node.next_node == @tail
+      node.next_node.next_node = nil
+      node.next_node = @tail
+      self
+    else
+      pop(node.next_node)
+    end
+  end
+
+  def empty?
+    size.zero?
+  end
+
+  def empty!
+    return if empty?
+
+    pop
+    empty!
+  end
+
+  def contains?(value)
+    each do |e|
+      return true if e.value == value
+    end
+    false
+  end
+
+  def find(value)
+    i = 0
+    each do |e|
+      return i if e.value == value
+
+      i += 1
+    end
+    "#{value} not found."
+  end
 end
 
 class Node
